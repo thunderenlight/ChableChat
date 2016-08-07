fe03156d453b282630cd7e583bb9dd8d0f86d8b5
thunderenlight <thunderenlight@gmail.com>
2016-08-07 15:59:40 -0500

    Adding devise gem and chat room model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'chat_rooms/index'
+
+  get 'chat_rooms/new'
+
+  get 'chat_rooms/create'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
